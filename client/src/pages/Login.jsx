5d0e562e2ef31fcfd56c7a56989785e5caa8831d
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 05:22:01 -0600

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,1 +1,32 @@
+import React, { useState } from "react";
+import "./Login.css";
 
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ email, password });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Email:</label>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password:</label>
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;
