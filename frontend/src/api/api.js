b1dc80cb1900a74bb0b8eac386f52f99a57d84f5
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 04:45:30 -0600

    Delete frontend directory

--- frontend/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
-export const registerUser = (userData) => API.post('/register', userData);
-export const loginUser = (userData) => API.post('/login', userData);
-export const getPlayerStats = (userId) => API.get(`/player/${userId}/stats`);
-export const getMarketplaceItems = () => API.get('/marketplace');
