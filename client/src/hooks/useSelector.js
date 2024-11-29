66b6d20f25edc1078bbada7a7010b6e55f47c5e6
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:46:08 -0600

    Update useSelector.js

--- client/src/hooks/useSelector.js
+++ client/src/hooks/useSelector.js
@@ -2,20 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function UserProfile() {
-  const user = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state.auth?.user); // Safe optional chaining to avoid errors
 
   if (!user) {
     return <p>Please log in.</p>;
   }
 
-  return <p>Welcome, {user.username}!</p>;
+  return <p>Welcome, {user.username || "Guest"}!</p>; // Fallback in case username is undefined
 }
 
 export default UserProfile;
-import { useSelector } from "react-redux";
-
-function UserProfile() {
-  const user = useSelector((state) => state.auth.user);
-
-  return <div>{user ? `Hello, ${user.username}` : "Not logged in"}</div>;
-}
