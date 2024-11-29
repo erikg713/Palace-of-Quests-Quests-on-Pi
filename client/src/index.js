5a95b7a41744833475bb7700270c6baf4bcf6650
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:42:23 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store'; // Adjust the path as necessary
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux"; // Redux Provider
