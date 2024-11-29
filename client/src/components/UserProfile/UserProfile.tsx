1af6f1ae04f1aa3f1bb1172574445fd78f139475
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:58:27 -0600

    Update UserProfile.tsx

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -1,29 +1,30 @@
-// UserProfile.tsx
-import React from 'react';
-import { useSelector } from 'react-redux';
-
-// Define the shape of the user object
-interface User {
-  username?: string; // username is optional
-}
-
-// Define the Redux state structure
-interface RootState {
-  auth?: {
-    user?: User;
-  };
-}
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUser, RootState } from './store';
 
 function UserProfile() {
-  // Use Redux state with proper type
-  const user = useSelector((state: RootState) => state.auth?.user);
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.auth.user);
+  const loading = useSelector((state: RootState) => state.auth.loading);
+  const error = useSelector((state: RootState) => state.auth.error);
 
-  // Conditional rendering
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   if (!user) {
     return <p>Please log in.</p>;
   }
 
-  return <p>Welcome, {user.username || "Guest"}!</p>;
+  return <p>Welcome, {user.username || 'Guest'}!</p>;
 }
 
 export default UserProfile;
