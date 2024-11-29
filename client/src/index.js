e2bb289c3df171ef79465974b6c63dd3f147dd59
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 05:25:32 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 
