f240ba4e75b459ddb194107c270380c7979a6a4c
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:17:55 -0600

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@db:5432/palace_of_quests"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "your-secret-key"
+class Config:
+    SECRET_KEY = "your-secret-key"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@db:5432/palace_of_quests"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "your-jwt-secret-key"
