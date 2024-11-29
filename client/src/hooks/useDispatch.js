4ad89124c177de82329f6bf3c0a48954bad798da
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:43:58 -0600

    Update useDispatch.js

--- client/src/hooks/useDispatch.js
+++ client/src/hooks/useDispatch.js
@@ -1,3 +1,30 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './authSlice';
+
+function AuthButtons() {
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    // Replace with actual login logic (e.g., API call)
+    const userData = { id: 1, username: 'testuser' };
+    const token = 'your_jwt_token';
+    dispatch(login({ user: userData, token }));
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+}
+
+export default AuthButtons;
 import { useDispatch } from "react-redux";
 import { login, logout } from "./authSlice";
 
