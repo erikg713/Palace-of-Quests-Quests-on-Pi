f033dd1a93412616c5703f46053576b59dacbf66
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:13:03 -0600

    Update auth.js

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,1 +1,11 @@
+import API from "./api";
 
+export const login = async (email, password) => {
+    const response = await API.post("/auth/login", { email, password });
+    return response.data;
+};
+
+export const register = async (userDetails) => {
+    const response = await API.post("/auth/register", userDetails);
+    return response.data;
+};
