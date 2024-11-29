8364773154a71238e56b29139422a610cbb9e764
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 07:56:37 -0600

    Update useAuth.js

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,3 +1,48 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchUser,
+  loginUser,
+  logoutUser,
+  updateUserProfile,
+  RootState,
+} from '../store';
+
+export const useAuth = () => {
+  const dispatch = useDispatch();
+
+  // State selectors
+  const user = useSelector((state: RootState) => state.auth.user);
+  const loading = useSelector((state: RootState) => state.auth.loading);
+  const error = useSelector((state: RootState) => state.auth.error);
+
+  // Fetch user on mount
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  // Action dispatchers
+  const login = (username: string, password: string) => {
+    dispatch(loginUser({ username, password }));
+  };
+
+  const logout = () => {
+    dispatch(logoutUser());
+  };
+
+  const updateProfile = (updatedData: { username?: string }) => {
+    dispatch(updateUserProfile(updatedData));
+  };
+
+  return {
+    user,
+    loading,
+    error,
+    login,
+    logout,
+    updateProfile,
+  };
+};
 import { useState, useEffect } from "react";
 
 const useAuth = () => {
