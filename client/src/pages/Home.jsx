0a5319b4eb4accf799937dfde36b319f393a2d86
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 05:21:13 -0600

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import "./Home.css";
 
+const Home = () => (
+    <main className="home">
+        <h1>Welcome to Palace of Quests</h1>
+        <p>Embark on your journey in the metaverse.</p>
+    </main>
+);
+
+export default Home;
