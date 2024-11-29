d4563a1e50fb307acd7aad395edf1b347e5f9cb6
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:41:38 -0600

    Update store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
+import authReducer from './authSlice'; // Adjust the path as necessary
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    // Include other reducers if you have them
   },
 });
 
