ef709d7120074842bb38ad7448d86ce5f2d87f39
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 06:44:36 -0600

    Update useSelector.js

--- client/src/hooks/useSelector.js
+++ client/src/hooks/useSelector.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function UserProfile() {
+  const user = useSelector((state) => state.auth.user);
+
+  if (!user) {
+    return <p>Please log in.</p>;
+  }
+
+  return <p>Welcome, {user.username}!</p>;
+}
+
+export default UserProfile;
 import { useSelector } from "react-redux";
 
 function UserProfile() {
