c07e120de13676478b5111067a05fb8f620ebf43
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-29 05:23:01 -0600

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,15 @@
+import axios from "axios";
 
+const API = axios.create({
+    baseURL: "http://localhost:5000/api",
+});
+
+export const login = async (email, password) => {
+    return API.post("/auth/login", { email, password });
+};
+
+export const fetchDashboardData = async () => {
+    return API.get("/dashboard");
+};
+
+export default API;
