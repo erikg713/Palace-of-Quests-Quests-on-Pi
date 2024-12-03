4fc7a0cf0ee796c862889d18c03ade1a6bd4b33b
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-03 08:32:17 -0600

    Update reportWebVitals.js

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,1 +1,15 @@
+// reportWebVitals.js
 
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+            getCLS(onPerfEntry);  // Cumulative Layout Shift
+            getFID(onPerfEntry);  // First Input Delay
+            getFCP(onPerfEntry);  // First Contentful Paint
+            getLCP(onPerfEntry);  // Largest Contentful Paint
+            getTTFB(onPerfEntry); // Time to First Byte
+        });
+    }
+};
+
+export default reportWebVitals;
