6986c8b20956512c33ad175e995efd3b9d8a7d8a
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-05 00:09:48 -0600

    Update Button.jsx

--- client/src/components/UI/Button.jsx
+++ client/src/components/UI/Button.jsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css'; // Assuming you have a CSS file for styling
 
+const Button = ({ onClick, label, type = 'button', className = '', disabled = false }) => {
+  return (
+    <button
+      type={type}
+      className={`btn ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+export default Button;
