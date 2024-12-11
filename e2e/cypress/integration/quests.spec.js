d43e9f44322d1483ccac9b9e7ad21e5cbcc211c1
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-11 05:06:48 -0600

    Update quests.spec.js

--- e2e/cypress/integration/quests.spec.js
+++ e2e/cypress/integration/quests.spec.js
@@ -1,1 +1,7 @@
-
+describe('Quest Progression', () => {
+  it('allows a player to complete a quest', () => {
+    cy.visit('/quests/1');
+    cy.get('button.complete-quest').click();
+    cy.contains('Quest Completed!');
+  });
+});
