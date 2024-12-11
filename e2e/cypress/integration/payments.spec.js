ca314c6764c2bb2d0c0658d1aa7c5c38288a61ac
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-11 05:33:57 -0600

    Update payments.spec.js

--- e2e/cypress/integration/payments.spec.js
+++ e2e/cypress/integration/payments.spec.js
@@ -1,1 +1,17 @@
+describe('Payment Flow', () => {
+  it('processes a successful Pi payment', () => {
+    cy.visit('/quests/1');
+    cy.get('button.purchase').click();
+    cy.get('#payment-modal').should('be.visible');
+    cy.get('button.confirm-payment').click();
+    cy.contains('Payment Successful!');
+  });
 
+  it('shows an error for insufficient balance', () => {
+    cy.visit('/quests/1');
+    cy.get('button.purchase').click();
+    cy.get('#payment-modal').should('be.visible');
+    cy.get('button.confirm-payment').click();
+    cy.contains('Insufficient Balance');
+  });
+});
