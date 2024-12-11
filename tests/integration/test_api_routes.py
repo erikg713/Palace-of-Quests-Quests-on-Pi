da1eafb93ab06ce8d39b7885e2193bbd793acc42
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-11 05:13:49 -0600

    Update test_api_routes.py

--- tests/integration/test_api_routes.py
+++ tests/integration/test_api_routes.py
@@ -1,4 +1,15 @@
-def test_get_quest(client):
-    response = client.get('/api/quests/1')
+import json
+
+def test_get_quests(client):
+    response = client.get('/api/quests')
     assert response.status_code == 200
-    assert response.json['id'] == 1
+    assert len(response.json) > 0
+
+def test_post_new_quest(client):
+    new_quest = {
+        "title": "Defeat the Dragon",
+        "level_required": 15
+    }
+    response = client.post('/api/quests', data=json.dumps(new_quest), content_type='application/json')
+    assert response.status_code == 201
+    assert response.json["title"] == "Defeat the Dragon"
