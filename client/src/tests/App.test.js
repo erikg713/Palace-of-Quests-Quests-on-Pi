e2405f77cd973fb4ba2b530ea82a5a49020d745e
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-11 05:28:32 -0600

    Update App.test.js

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,1 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
 
+test('renders the home page', () => {
+  render(<App />);
+  const homeElement = screen.getByText(/Welcome to Palace of Quests/i);
+  expect(homeElement).toBeInTheDocument();
+});
