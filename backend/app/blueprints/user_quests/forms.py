7ef51fc68995f7edd02b52c4c55105fb89b8e105
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:42:33 -0500

    Update forms.py

--- backend/app/blueprints/user_quests/forms.py
+++ backend/app/blueprints/user_quests/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+class UserQuestForm(FlaskForm):
+    quest_id = IntegerField('Quest ID', validators=[DataRequired()])
+    submit = SubmitField('Accept Quest')
 
