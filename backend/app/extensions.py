fce26e289c12f66694aa0403eddbeaf63ec41255
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 23:06:33 -0500

    Update extensions.py

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -8,3 +8,11 @@ db = SQLAlchemy()
 
 # Initialize the CORS object
 cors = CORS()
+# app/extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf import CSRFProtect
+
+db = SQLAlchemy()
+migrate = Migrate()
+csrf = CSRFProtect()
