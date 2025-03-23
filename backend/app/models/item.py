a606a959197fba22049cb06205f85fbb516bf00d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:51:13 -0500

    Update item.py

--- backend/app/models/item.py
+++ backend/app/models/item.py
@@ -1,1 +1,11 @@
+# app/models/item.py
+from app import db
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner = db.relationship('User', backref='items')
 
