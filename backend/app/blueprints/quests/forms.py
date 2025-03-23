370653bc257ad4442d812d48b2ba58a7d37cb151
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:28:18 -0500

    Update forms.py

--- backend/app/blueprints/quests/forms.py
+++ backend/app/blueprints/quests/forms.py
@@ -1,1 +1,11 @@
+# app/blueprints/quests/forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
+
+class QuestForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    reward = IntegerField('Reward', validators=[DataRequired(), NumberRange(min=1)])
+    submit = SubmitField('Create Quest')
 
