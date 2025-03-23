33e1ffdf29288b911e3dc898e4c77e55f79d537d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:39:34 -0500

    Update forms.py

--- backend/app/blueprints/users/forms.py
+++ backend/app/blueprints/users/forms.py
@@ -1,1 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
 
