a8397691ba133b88fdeccdbdf8b4a13c2994777f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:50:45 -0500

    Update quest.py

--- backend/app/models/quest.py
+++ backend/app/models/quest.py
@@ -1,1 +1,10 @@
+# app/models/quest.py
+from app import db
+
+class Quest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    reward = db.Column(db.Float, nullable=False)
+    user_quests = db.relationship('UserQuest', backref='quest', lazy=True)
 
