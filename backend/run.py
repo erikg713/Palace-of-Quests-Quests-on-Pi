0c96bfc0b8b2be9b7ec64793b909e577261c0c0e
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 23:13:28 -0500

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,16 @@
 from app import create_app
+from app.extensions import db, migrate, csrf
 from app.middleware.logger import LoggerMiddleware
 from app.middleware.rate_limiter import RateLimiterMiddleware
 
 app = create_app()
 app.wsgi_app = LoggerMiddleware(app.wsgi_app)
 app.wsgi_app = RateLimiterMiddleware(app.wsgi_app, limit=100, period=60)
+
+# Initialize extensions
+db.init_app(app)
+migrate.init_app(app, db)
+csrf.init_app(app)
 
 if __name__ == '__main__':
     app.run()
