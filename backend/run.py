3ce8b142d50331bbaf48c9160555229c71c89fce
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:58:15 -0500

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,8 @@
-# run.py
 from app import create_app
+from app.middleware.logger import LoggerMiddleware
 
 app = create_app()
+app.wsgi_app = LoggerMiddleware(app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
