8538acd36edb54bc01c4030734386394aa1f88d3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 22:51:44 -0500

    Update transaction.py

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -1,1 +1,10 @@
+from app import db
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
 
