a63cae5ffb3057571b033af693e31ed8cc268602
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-22 23:01:01 -0500

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,10 @@
 from app import create_app
 from app.middleware.logger import LoggerMiddleware
+from app.middleware.rate_limiter import RateLimiterMiddleware
 
 app = create_app()
 app.wsgi_app = LoggerMiddleware(app.wsgi_app)
+app.wsgi_app = RateLimiterMiddleware(app.wsgi_app, limit=100, period=60)
 
 if __name__ == '__main__':
     app.run()
